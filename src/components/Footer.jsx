a7ae397e4ef3db1e1f5bec1004d1057d2e1b7017
anikobakhidze <kobakhidzeana23@gmail.com>
2024-03-17 14:30:36 +0400

    refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 import footerLogo from "../assets/images/footerLogo.png";
 import { VscArrowCircleRight } from "react-icons/vsc";
-import { LiaFacebook } from "react-icons/lia";
-import { CiTwitter, CiYoutube } from "react-icons/ci";
 import Logo from "./Logo";
+import SocialIcons from "./SocialIcons";
 function Footer() {
   return (
     <footer className="flex place-items-center justify-around bg-[#1c5858] text-white pb-6">
@@ -26,6 +25,14 @@ function Footer() {
                 Privacy & Policy
               </a>
             </li>
+            <li className="my-2">
+              <a
+                href="/"
+                className="transition-colors duration-300 hover:text-gray-300"
+              >
+                Contact
+              </a>
+            </li>
           </ul>
         </nav>
         <p>&copy; 2024</p>
@@ -41,17 +48,7 @@ function Footer() {
           />
           <VscArrowCircleRight className=" absolute w-6 h-6 bottom-2 right-1.5 text-gray-800 cursor-pointer transition-all hover:text-[#53b1b1] hover:scale-125" />
         </form>
-        <div className="flex pt-4 gap-2.5">
-          <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-            <LiaFacebook className="w-8 h-8 cursor-pointer hover:scale-125 transition-all duration-300" />
-          </a>
-          <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
-            <CiTwitter className="w-8 h-8 cursor-pointer hover:scale-125 transition-all duration-300" />
-          </a>
-          <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
-            <CiYoutube className="w-8 h-8 cursor-pointer hover:scale-125 transition-all duration-300" />
-          </a>
-        </div>
+        <SocialIcons />
       </div>
     </footer>
   );
