a49ca04bc26910e70c134b9c06f0d84613c5bdd6
anikobakhidze <kobakhidzeana23@gmail.com>
2024-03-26 21:00:42 +0400

    change UserProfileInfo component

--- src/components/UserProfileInfo.jsx
+++ src/components/UserProfileInfo.jsx
@@ -1,44 +1,11 @@
 import React from "react";
 
-function UserProfileInfo() {
+function UserProfileInfo({ label, userInfo }) {
   return (
-    <fieldset className="flex flex-col gap-4 w-2/6 h-80 ">
-      <legend className="text-2xl font-bold text-teal-800 mb-4 w-full ">
-        About
-      </legend>
-      <label htmlFor="name" className="text-xl font-semibold">
-        Name
-      </label>
-      <input
-        className="h-10 text-lg pl-2 rounded-xl outline-none "
-        id="name"
-        type="text"
-        value="Name"
-        placeholder="Name"
-        readOnly
-      />
-      <label htmlFor="surname" className="text-xl font-semibold">
-        Surname
-      </label>
-      <input
-        className="h-10 text-lg pl-2 rounded-xl outline-none "
-        id="surname"
-        type="text"
-        value="Surname"
-        placeholder="Surname"
-        readOnly
-      />
-      <label htmlFor="email" className="text-xl font-semibold">
-        Email
-      </label>
-      <input
-        className="h-10 text-lg pl-2 rounded-xl outline-none "
-        id="email"
-        type="Email"
-        value="Email"
-        readOnly
-      />
-    </fieldset>
+    <>
+      <h5 className="text-xl font-semibold">{label}</h5>
+      <p className="h-10 text-lg  rounded-xl "> {userInfo}</p>
+    </>
   );
 }
 
--- src/components/UserProfileSettings.jsx
+++ src/components/UserProfileSettings.jsx
@@ -8,15 +8,22 @@ function UserProfileSettings() {
       <h2 className="text-center text-3xl font-bold text-teal-800 mb-12">
         User Profile
       </h2>
-      <form className="text-medium-green w-1100 mx-auto flex bg-slate-100 p-16 justify-between rounded-3xl items-center">
+      <div className="text-medium-green w-1100 mx-auto flex bg-slate-100 p-16 justify-between rounded-3xl items-center">
         <img
           src={defaultUserImage}
           alt="user"
           className="w-60 h-60 rounded-full"
         />
-        <UserProfileInfo />
+        <div className="flex flex-col gap-4 w-2/6 h-80 ">
+          <h3 className="text-2xl font-bold text-teal-800 mb-4 w-full ">
+            About
+          </h3>
+          <UserProfileInfo label="Name" userInfo="Anna" />
+          <UserProfileInfo label="Surname" userInfo="Kobakhidze" />
+          <UserProfileInfo label="Email" userInfo="kobakhidzeana23@gmail.com" />
+        </div>
         <PasswordResetForm />
-      </form>
+      </div>
     </section>
   );
 }
