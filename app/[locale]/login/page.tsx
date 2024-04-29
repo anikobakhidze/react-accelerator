628858b68724e9672395f5e94cd0694d09f4e2ca
anikobakhidze <kobakhidzeana23@gmail.com>
2024-04-29 17:59:33 +0400

    add gif

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -4,6 +4,8 @@ import ThemeSwitch from "@/components/sharedComponents/UI/ThemeSwitch";
 import initTranslations from "@/app/i18n";
 import TranslationsProvider from "../../../components/sharedComponents/Language/TranslationProvides";
 import LanguageChanger from "@/components/sharedComponents/Language/LanguageChanger";
+import gif from "../../../public/gif/gif.gif";
+import Image from "next/image";
 export default async function LogIn({ params: { locale } }: IParamsProps) {
   const i18nNamespaces = ["common"];
   const { t, resources } = await initTranslations(
@@ -19,13 +21,33 @@ export default async function LogIn({ params: { locale } }: IParamsProps) {
       locale={locale}
       resources={resources}
     >
-      <main className="w-full h-[100vh] flex flex-col items-center justify-around bg-medium-green dark:bg-slate-800 ">
-        <div className="flex items-center ">
+      <main className="w-full h-[100vh] flex flex-col items-center justify-around bg-[#b2babe] dark:bg-slate-800 ">
+        {/* <div className="flex items-center ">
           <h1 className="text-lg mr-2 text-white">{t("theme")}</h1>
           <ThemeSwitch />
           <LanguageChanger />
         </div>
-        <LogInForm />
+        <LogInForm /> */}
+        <section className="flex ">
+          <div>
+            {/* <h1 className="text-lg mr-2 text-white">{t("theme")}</h1> */}
+            <ThemeSwitch />
+            <LanguageChanger />
+            <div>
+              <Image
+                src={gif}
+                alt="girl is shopping"
+                width={800}
+                height={800}
+                unoptimized
+                className="rounded-3xl"
+              />
+            </div>
+          </div>
+          <div>
+            <LogInForm />
+          </div>
+        </section>
       </main>
     </TranslationsProvider>
   );
