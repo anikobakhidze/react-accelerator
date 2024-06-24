01ebfd52df1f92a561ccbdffa14052632451ffb1
anikobakhidze <kobakhidzeana23@gmail.com>
2024-06-25 02:17:23 +0400

    fix checkout page

--- app/[locale]/(dashboard)/checkout/page.tsx
+++ app/[locale]/(dashboard)/checkout/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { getSession } from "@auth0/nextjs-auth0";
 import CheckoutDetails from "../../../../components/checkoutPage/CheckoutDetails";
-import { getCartItems } from "@/api";
+import { getUserCart } from "@/api";
 async function CheckoutPage() {
   const session = await getSession();
   const user = session?.user;
   const userId = user?.sub;
   const userEmail = user?.email;
-  const products = await getCartItems(userId);
+  const products = await getUserCart(userId);
   return (
     <CheckoutDetails
       userId={userId}
